0c09903e24ffb12456f5ca55fdafe9988d999e86
Paul Jensen <paulbjensen@gmail.com>
2016-09-29 06:18:00 +0100

    Got the app generator creating a folder

--- test/lib/generator/index.test.js
+++ test/lib/generator/index.test.js
@@ -2,38 +2,33 @@
 
 const assert = require('assert');
 const fs = require('fs');
-const generator = require('');
+const generator = require('../../../lib/generator');
 const path = require('path');
 
 describe('generator', () => {
 
-	let folderName = 'flight';
-	let appFolderPath = path.join(process.cwd(), 'test','sandbox', folderName);
+	const folderName = 'flight';
+	const appFolderLocation = path.join(process.cwd(), 'test','sandbox');
+	const appFolderPath = path.join(appFolderLocation, folderName);
 
 	beforeEach((done) => {
 		fs.exists(appFolderPath, (exists) => {
 			if (!exists) return done();
-			fs.unlink(appFolderPath, done);
+			fs.rmdir(appFolderPath, done);
 		});
 	});
-	// Remove any folders that we need to create during the process of testing
 
   it('should create an app skeleton for a developer', (done) => {
 		fs.exists(appFolderPath, (exists) => {
 			assert.equal(false, exists);
-			generator.generate(appFolderPath, (err) => {
+			generator.generate(folderName, appFolderLocation, (err) => {
 				assert.equal(null, err);
 				fs.exists(appFolderPath, (nowExists) => {
 					assert(nowExists);
-					
+					done();
 				});
 			});
-			// Call the generator
-
 		});
-		// Assert that a folder with the expected name does not yet exist
-		// Run app generator with a given name
-		// Assert that a folder with the expected name exists
 		// Assert that an app.js file exists
 		// Assert that the file has the expected content
 		// Assert that a package.json file exists
